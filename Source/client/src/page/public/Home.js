294497f66c48da405b7003487fd12c324c343918
tungdang <tungdang2k0@gmail.com>
2023-05-01 20:44:30 +0700

    m

--- Source/client/src/page/public/Home.js
+++ Source/client/src/page/public/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 import { Section, Banner } from '../../components'
-import Slider from "react-slick";
 import background from '../../asset/image/background.svg'
 import icons from '../../ultis/icons'
 
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper";
 const Home = () => {
 
     const { BsFillPlayFill } = icons
@@ -43,36 +49,30 @@ const Home = () => {
         <div className='flex flex-col w-full '>
             <Banner />
 
+
+
             <div className="px-12 w-full">
                 <div className="flex flex-col mt-4">
                     <p className='text-white'>Mới phát hành</p>
                     <div className='text-white'>
-                        <Slider {...settings}>
-                            <div>
-                                <h3>1</h3>
-                            </div>
-                            <div>
-                                <h3>2</h3>
-                            </div>
-                            <div>
-                                <h3>3</h3>
-                            </div>
-                            <div>
-                                <h3>4</h3>
-                            </div>
-                            <div>
-                                <h3>5</h3>
-                            </div>
-                            <div>
-                                <h3>6</h3>
-                            </div>
-                            <div>
-                                <h3>7</h3>
-                            </div>
-                            <div>
-                                <h3>8</h3>
-                            </div>
-                        </Slider>
+                        <Swiper
+                            slidesPerView={6}
+                            spaceBetween={30}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            modules={[Pagination]}
+                        >
+                            <SwiperSlide><Section height={136} img={background} /></SwiperSlide>
+                            <SwiperSlide><Section height={136} img={background} /></SwiperSlide>
+                            <SwiperSlide><Section height={136} img={background} /></SwiperSlide>
+                            <SwiperSlide><Section height={136} img={background} /></SwiperSlide>
+                            <SwiperSlide><Section height={136} img={background} /></SwiperSlide>
+                            <SwiperSlide><Section height={136} img={background} /></SwiperSlide>
+                            <SwiperSlide><Section height={136} img={background} /></SwiperSlide>
+                            <SwiperSlide><Section height={136} img={background} /></SwiperSlide>
+                            <SwiperSlide><Section height={136} img={background} /></SwiperSlide>
+                        </Swiper>
                     </div>
                 </div>
 
