335f4f722f0a3fe63079c85ff5857ce95790c053
Thái <hoangthai.txt@gmail.com>
2023-05-06 10:30:52 +0700

    edit query getUsername

--- Source/server/src/controllers/user.controller.js
+++ Source/server/src/controllers/user.controller.js
@@ -182,7 +182,7 @@ const updateUserByUser = async (req, res) => {
 
 const getUserByUsername = async (req, res) => {
     try {
-        const { username } = req.body
+        const { username } = req.query
         const checkUserName = await userModel.findOne({ username }).select('username id displayName')
         if (!checkUserName) return responseHandler.notfound(res, 'Không tìm thấy username')
         responseHandler.ok(res, checkUserName)
