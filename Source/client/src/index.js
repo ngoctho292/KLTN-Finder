f7defd0a17745012831a6388505b961cca752a29
tungdang <tungdang2k0@gmail.com>
2023-05-13 20:44:11 +0700

    fix merge

--- Source/client/src/index.js
+++ Source/client/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
 import { BrowserRouter, } from 'react-router-dom'
 import reduxConfig from './redux';
-import { Provider } from 'react-redux';
+
 
 const store = reduxConfig()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter><App /></BrowserRouter>
+  <BrowserRouter>
+    <Provider store={store}> <App /></Provider>
+  </BrowserRouter>
 );
 
