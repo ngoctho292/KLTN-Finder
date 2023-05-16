c38d17eccb4dc8fd78d8c0e3c746b9fc94a449dd
tungdang <tungdang2k0@gmail.com>
2023-05-16 20:05:32 +0700

    f

--- Source/client/src/components/Banner.js
+++ Source/client/src/components/Banner.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactPlayer from 'react-player';
 
 import movies from '../asset/image/logomoives.png'
 import background from '../asset/image/background.png'
@@ -6,11 +7,37 @@ import icons from '../ultis/icons'
 
 const Banner = ({ banerModal, data }) => {
     const { BsFillPlayFill, SlLike, AiOutlinePlus, AiOutlineExclamationCircle } = icons
+    const [playing, setPlaying] = useState(true);
+
+    const handleVideoEnd = () => {
+        setPlaying(false);
+    };
+
     return (
         <div className='relative'>
-            <img src={banerModal ? data?.poster_path[0]?.path : background} alt="background" className='w-full object-cover mt-[-80px] z-0' />
-            {/* 
-            <video src="https://www.youtube.com/watch?v=pQh775SP_dA&pp=ygUbVHJhaWxlciBIaeG7h3Agc8SpIMOhbyDEkWVu" autoPlay ></video> */}
+
+            <img src={banerModal ? data?.poster_path[0]?.path : background} alt="background" className={`w-full object-cover z-0 ${banerModal ? '' : 'h-[600px]'}`} />
+            {/* data?.trailer ? data?.trailer :  */}
+            {/* {playing ? <ReactPlayer
+                url={`${'https://www.youtube.com/embed/4S9ih9boBOw'}`}
+                controls={false}
+                playing={true}
+                loop={false}
+                width="100%"
+                height="600px"
+                onEnded={handleVideoEnd}
+                config={{
+                    youtube: {
+                        playerVars: {
+                            modestbranding: 1,
+                            rel: 0,
+                            showinfo: 0,
+                            iv_load_policy: 3,
+                            origin: 'http://192.168.94.1:3000'
+                        },
+                    },
+                }}
+            /> : <img src={banerModal ? data?.poster_path[0]?.path : background} alt="background" className={`w-full object-cover z-0 ${banerModal ? '' : 'h-[600px]'}`} />} */}
 
             <div className='px-12 absolute top-0 left-0 bottom-0 right-0 bg-gradient-to-t from-[rgba(0,0,0,0.5)] to-transparent  text-white'>
                 <div className="absolute top-[80px] pt-8 text-white">
--- Source/client/src/components/SectionList.js
+++ Source/client/src/components/SectionList.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Section from './Section'
 const SectionList = () => {
     return (
         <div>
